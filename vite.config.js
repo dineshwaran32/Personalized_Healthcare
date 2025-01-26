2a66e7722e46d1261536cbe7753fdb5b0e49fa22
Pragadeesh Nehru <pragadeeshxenocrate@gmail.com>
2025-01-26 06:12:06 +0000

    tailwind config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss(),],
+  server: {
+    allowedHosts: ['5173-pragadeeshn-personalize-ae8s9va59jg.ws-us117.gitpod.io']
+  }
 })
